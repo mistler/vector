1ea42f8ebf82b1e86c042526e0387cf6fdebfff9
Mikhail Istomin <mistler.wrk@gmail.com>
2023-10-03 14:13:35 +0700

    all: init, push_back, expanding - work

--- main.c
+++ main.c
@@ -6,6 +6,7 @@
 struct Vector {
     int *ptr;
     size_t currentAmount;
+    size_t allocatedAmount;
 };
 
 void vector_init(struct Vector *v) {
@@ -16,15 +17,53 @@ void vector_init(struct Vector *v) {
         printf("PANIC\n");
         return;
     }
+    v->allocatedAmount = INITIAL_VEC_CAPACITY;
+}
+
+void expandMemoryIfNeeded(struct Vector *v) {
+    if (v->currentAmount == v->allocatedAmount) {
+        const size_t newAmount = v->allocatedAmount * 2;
+
+        int *tmpPtr = malloc(sizeof(int) * newAmount);
+        if (tmpPtr == NULL) {
+            printf("PANIC\n");
+            return;
+        }
+
+        // Copy
+        for (size_t i = 0; i < v->currentAmount; ++i) {
+            tmpPtr[i] = v->ptr[i];
+        }
+
+        // Free old
+        free(v->ptr);
+
+        // Assing new to vector
+        v->ptr = tmpPtr;
+
+        v->allocatedAmount = newAmount;
+    }
 }
 
 void vector_push_back(struct Vector *v, int elem) {
+    expandMemoryIfNeeded(v);
     v->ptr[v->currentAmount] = elem; // *(v->ptr + v->currentAmount) = elem;
     v->currentAmount += 1;
 }
 
 size_t vector_size(struct Vector *v) {
     return v->currentAmount;
+}
+
+size_t vector_capacity(struct Vector *v) {
+    return v->allocatedAmount;
+}
+
+void vector_debug_print(struct Vector *v) {
+    for(size_t i = 0; i < v->currentAmount; ++i) {
+        printf("%d ", v->ptr[i]);
+    }
+    printf("\n");
 }
 
 int vector_back(struct Vector *v) {
@@ -40,26 +79,28 @@ int main(void) {
 
     vector_init(&v);
 
-    vector_push_back(&v, 4);
-    vector_push_back(&v, 5);
+    while(1) {
+        int newElem;
+        scanf("%d", &newElem);
+        vector_push_back(&v, newElem);
 
-    printf("vector size: %zu\n", vector_size(&v)); // 2
-    printf("vec back: %d\n", vector_back(&v)); // 5
+        printf("vector size: %zu\n", vector_size(&v));
+        printf("vector capacity: %zu\n", vector_capacity(&v));
+        printf("vec back: %d\n", vector_back(&v));
+        vector_debug_print(&v);
+    }
 
     vector_deinit(&v);
 
 
-    // TODO(mistler): init vector.
-    // TODO(mistler): add elems (i-th, last, first).
-    // TODO(mistler): access elems (i-th, last, first).
+    // TODO(mistler): add elems (i-th, !last, first).
+    // TODO(mistler): access elems (i-th, !last, first).
     // TODO(mistler): remove elems (i-th, last, first).
-    // TODO(mistler): lenght of vector.
     // TODO(mistler): is empty?
     // TODO(mistler): clear.
     //
     //
     // TODO(katya): sort.
     // TODO(petr): search elem.
-    // TODO(mistler): deinit vector.
     return 0;
 }
