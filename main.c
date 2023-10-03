497e842d3006f20b312e94bb666a7fad77fface8
Mikhail Istomin <mistler.wrk@gmail.com>
2023-10-03 13:22:32 +0700

    all: added initial todos

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+struct Vector {
+};
+
 int main(void) {
-    printf("Hello world\n");
+    struct Vector v;
+    // TODO(mistler): init vector.
+    // TODO(mistler): add elems (i-th, last, first).
+    // TODO(mistler): access elems (i-th, last, first).
+    // TODO(mistler): remove elems (i-th, last, first).
+    // TODO(mistler): lenght of vector.
+    // TODO(mistler): is empty?
+    // TODO(mistler): clear.
+    //
+    //
+    // TODO(katya): sort.
+    // TODO(petr): search elem.
+    // TODO(mistler): deinit vector.
     return 0;
 }
