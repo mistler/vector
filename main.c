2dce8e2b159ccb8ebf1b531f771c096da2714b16
Mikhail Istomin <mistler.wrk@gmail.com>
2023-10-03 13:35:16 +0700

    all: added sample of vector usage

--- main.c
+++ main.c
@@ -5,6 +5,15 @@ struct Vector {
 
 int main(void) {
     struct Vector v;
+
+    vector_add(&v, 4);
+    vector_add(&v, 5);
+
+    printf("vector size: %zu\n", vector_size(&v)); // 2
+    printf("vec[0]: %d\n", vector_get(&v, 0)); // 4
+
+
+
     // TODO(mistler): init vector.
     // TODO(mistler): add elems (i-th, last, first).
     // TODO(mistler): access elems (i-th, last, first).
